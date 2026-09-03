7fdf9da1148bcec8f4d082e725d8d3d0adc706b0
tharu-96 <bibektharu412@gmail.com>
2026-09-03 11:30:51 +0545

    Fix Render deployment configuration

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py shell -c "
 from django.db import connection
 
